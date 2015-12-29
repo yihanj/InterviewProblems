fc74dc9f6181b9316307aee92721995831e83d19
yihanj <jiangyihanin1990@gmail.com>
2015-12-29 11:43:21 -0800

    Create serialize_deserialize_json.py

---
+++ serialize_deserialize_json.py
@@ -0,0 +1,25 @@
+import json
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def tuplify(root):
+            return root and (root.val, tuplify(root.left), tuplify(root.right))
+        return json.dumps(tuplify(root))
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def detuplify(t):
+            if t:
+                root = TreeNode(t[0])
+                root.left = detuplify(t[1])
+                root.right = detuplify(t[2])
+                return root
+        return detuplify(json.loads(data))
